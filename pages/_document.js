15a02902825a56f1ada4bc3c8b90a8b262364c4e
Ahad Sohail <ahadsohail4@gmail.com>
2023-08-08 10:28:05 +0500

    Google Analytics Added

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,31 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import FacebookPixel from '../components/common/facebook/FacebookPixel'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html>
       <Head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-EXJNX1X5JW" />
+        <Script id="google-analytics">
+          {
+            `
+          window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-EXJNX1X5JW');
+          `
+          }
+
+        </Script>
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
+
+
     </Html>
   )
 }
