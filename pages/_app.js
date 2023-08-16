0c7267e0092d67ed6f9f70b3c6a9eb7872d42fd6
Ahad Sohail <ahadsohail4@gmail.com>
2023-08-16 11:36:53 +0500

    GA Upgraded

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,6 @@ function MyApp({ Component, pageProps }) {
   }, []);
   return <>
     <Component {...pageProps} />
-    <Google />
     <FacebookPixel />
   </>
 }
--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,18 @@ import Script from 'next/script'
 export default function Document() {
   return (
     <Html>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-EXJNX1X5JW"></Script>
+      <Script id='googleScript' >
+        {
+          `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+  
+          gtag('config', 'G-EXJNX1X5JW');
+          `
+        }
+      </Script>
       <Head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </Head>
@@ -11,8 +23,6 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
-
-
     </Html>
   )
 }
