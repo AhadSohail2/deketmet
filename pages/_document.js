916f6ca122d0818fc012e6b450a529e234c08c72
Ahad Sohail <ahadsohail4@gmail.com>
2023-08-16 11:38:49 +0500

    UP

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,9 @@ import Script from 'next/script'
 export default function Document() {
   return (
     <Html>
+      <Head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+      </Head>
       <Script async src="https://www.googletagmanager.com/gtag/js?id=G-EXJNX1X5JW"></Script>
       <Script id='googleScript' >
         {
@@ -16,9 +19,6 @@ export default function Document() {
           `
         }
       </Script>
-      <Head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
-      </Head>
       <body>
         <Main />
         <NextScript />
