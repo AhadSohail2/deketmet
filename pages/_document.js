ccd5c0ebacf43d783f5edc9fc91d7a6e1a9e067f
Ahad Sohail <ahadsohail4@gmail.com>
2023-09-07 14:04:05 +0500

    Add Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -6,19 +6,19 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-P19W527YQ5"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments);}
+         gtag('js', new Date());
+
+         gtag('config', 'G-P19W527YQ5');
+        `}} />
+
       </Head>
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-EXJNX1X5JW"></Script>
-      <Script id='googleScript' >
-        {
-          `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-  
-          gtag('config', 'G-EXJNX1X5JW');
-          `
-        }
-      </Script>
+
+
       <body>
         <Main />
         <NextScript />
